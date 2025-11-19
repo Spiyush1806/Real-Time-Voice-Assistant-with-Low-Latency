04bdd6616403bdcfe1841a6d72011558b05d7781
PIYUSH KUMAR <piyushkumar3@iisc.ac.in>
2025-11-19 23:00:49 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-## for a given query tell ollama to generate a response from the model
-
-response = requests.post("http://localhost:11434/v1/chat/completions", json={
-    "model": "gemma3:4b",
-    "messages": [{"role": "user", "content": "What is the capital of France?"}]
-})
-print(response.json())
